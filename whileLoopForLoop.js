59c9c9b9528215de074273ca365ef2fea5ead14c
shohel-ranaC <akashshohel422@gmail.com>
2022-09-05 12:23:54 +0600

    while loop for loop and switch case

---
+++ whileLoopForLoop.js
@@ -0,0 +1,44 @@
+let num = 10;
+while(num < 20){
+   
+    num++;
+    const element = num * 2;
+    console.log(element);
+}
+// console.log(num);
+
+
+var numbers = 10;
+//let sum = 0;
+for (var i = 0; i < numbers; i++) {
+    const sum = i * 2;
+    console.log(sum);
+    
+}
+
+let number = [10, 20, 30, 40, 50, 60, 70, 80, 90];
+for (let i = 0; i < number.length; i++) {
+    const arrayDisplay = number[i];
+    console.log(arrayDisplay);
+}
+
+
+// switch case practice.
+let number1 = 20;
+switch(number1){
+    case 5:
+        console.log('yes number is 5');
+        break;
+    case 10 :
+        console.log('yes number is got 10');
+        break;
+    case 20:
+        console.log('yes number is got 20') ;
+        break;
+    default:
+        console.log('input is empty ')  ; 
+        break;    
+}
+
+
+
